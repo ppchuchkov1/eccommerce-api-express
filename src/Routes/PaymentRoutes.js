6ecbcbcd6bf63b960c2a3d6392ff7cf1218a4d03
ppchuchkov1 <ppchuchkov1@gmail.com>
2024-10-13 02:55:58 +0300

    import body parser

--- src/Routes/PaymentRoutes.js
+++ src/Routes/PaymentRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser"); // Добавен импорт на body-parser
 const { stripePayment, webhook } = require("../Controllers/PaymentController");
 
 const router = express.Router();
 
+// Middleware за обработка на суровото тяло
 const rawBodyBuffer = (req, res, buf, encoding) => {
   if (encoding !== "utf-8") {
     return;
@@ -10,7 +12,9 @@ const rawBodyBuffer = (req, res, buf, encoding) => {
   req.rawBody = buf.toString(encoding);
 };
 
+// Маршрут за създаване на сесия за плащане
 router.post("/create-checkout-session", stripePayment);
+// Маршрут за webhook
 router.post("/webhook", bodyParser.json({ verify: rawBodyBuffer }), webhook);
 
 module.exports = router;
