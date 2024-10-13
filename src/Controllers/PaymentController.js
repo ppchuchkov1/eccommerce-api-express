e08f1b4e1209493c4e41f811cfc306fd85301500
ppchuchkov1 <ppchuchkov1@gmail.com>
2024-10-13 22:54:33 +0300

    Add webhook stripe success email send

--- src/Controllers/PaymentController.js
+++ src/Controllers/PaymentController.js
@@ -25,37 +25,10 @@ const stripePayment = async (req, res) => {
       mode: "payment",
       success_url: `https://stipe-react.netlify.app/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `https://stipe-react.netlify.app/cancel`,
-    });
-
-    // Изпращане на потвърдителен имейл
-    const htmlTemplatePath = path.join(
-      __dirname,
-      "../payment-email-template.html"
-    );
-
-    fs.readFile(htmlTemplatePath, "utf-8", (err, htmlContent) => {
-      if (err) {
-        console.error("Error reading HTML file:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-
-      // Изпращане на имейл
-      const mailOptions = {
-        from: process.env.GMAIL_USER,
-        to: customerEmail,
-        subject: "Payment Successful",
-        text: `Thank you for your purchase! Your payment was successful. Session ID: ${session.id}`,
-        html: htmlContent,
-      };
-
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error("Error sending email:", error);
-          return res.status(500).json({ error: "Failed to send email." });
-        } else {
-          console.log("Email sent: " + info.response);
-        }
-      });
+      // Store the customer email in the metadata
+      metadata: {
+        customerEmail, // Store customer email
+      },
     });
 
     res.status(200).json({ id: session.id });
@@ -65,6 +38,68 @@ const stripePayment = async (req, res) => {
   }
 };
 
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+const stripeWebHook = async (req, res) => {
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const checkoutSessionCompleted = event.data.object;
+
+      // Get the customer email from metadata
+      const customerEmail = checkoutSessionCompleted.metadata.customerEmail;
+
+      // Read the HTML email template
+      const htmlTemplatePath = path.join(
+        __dirname,
+        "../payment-email-template.html"
+      );
+
+      fs.readFile(htmlTemplatePath, "utf-8", (err, htmlContent) => {
+        if (err) {
+          console.error("Error reading HTML file:", err);
+          return; // You can log or handle the error here if needed
+        }
+
+        // Send the confirmation email
+        const mailOptions = {
+          from: process.env.GMAIL_USER,
+          to: customerEmail,
+          subject: "Payment Successful",
+          text: `Thank you for your purchase! Your payment was successful. Session ID: ${checkoutSessionCompleted.id}`,
+          html: htmlContent,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error("Error sending email:", error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+      });
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  res.send();
+};
+
 module.exports = {
   stripePayment,
+  stripeWebHook,
 };
--- src/Routes/PaymentRoutes.js
+++ src/Routes/PaymentRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { stripePayment } = require("../Controllers/PaymentController");
+const {
+  stripePayment,
+  stripeWebHook,
+} = require("../Controllers/PaymentController");
 
 const router = express.Router();
 
 // Маршрут за създаване на сесия за плащане
 router.post("/create-checkout-session", stripePayment);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebHook
+);
 
 module.exports = router;
